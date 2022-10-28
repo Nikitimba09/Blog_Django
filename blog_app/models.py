cfaec1a93819f97e77be38f547c534d9ba1263d0
Nikita <nickit.schatalow@yandex.ru>
2022-10-28 20:31:15 +0400

    update models

--- blog_app/models.py
+++ blog_app/models.py
@@ -8,16 +8,31 @@ class Post(models.Model):
         ('draft', 'Draft'),
         ('published', 'Published'),
     )
-    title = models.CharField(max_length=250)  #Это поле является CharField, которое преобразуется в столбец VARCHAR в базе данных SQL.
-    slug = models.SlugField(max_length=250, unique_for_date='published') #slug: это поле, предназначенное для использования в URL-адресах.
-    author = models.ForeignKey(User, related_name='blog_posts', on_delete=models.CASCADE) #author: это поле ForeignKey. В
-    # этом поле
-    # определяется отношение «многие к одному». Мы сообщаем о том, что каждая запись написана пользователем, и пользователь можетсоздать сколько угодно постов(posts).
-    body = models.TextField() #Это поле является полем TextField, которое преобразуется в текстовый столбец базы данных SQL
-    publish = models.DateTimeField(default=timezone.now()) #publish : этот параметр DateTime указывает, когда была опубликована запись.
-    created = models.DateTimeField(auto_now_add=True) #created : этот DateTime параметр указывает, когда был создан пост. Поскольку мы используем auto_now_add здесь, дата будет автоматически добавлена при создании объекта.
-    updated = models.DateTimeField(auto_now=True) #updated : этот DateTime параметр указывает на последний момент обновления этой записи.
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft') #status : это поле для отображения статуса записи(опубликован\неопубликован).
+    # Это поле является CharField, которое преобразуется в столбец VARCHAR в базе данных SQL.
+    title = models.CharField(max_length=250)
+
+    # slug: это поле, предназначенное для использования в URL-адресах.
+    slug = models.SlugField(max_length=250, unique_for_date='published')
+
+    # Author: это поле ForeignKey. В этом поле определяется отношение «многие к одному». Мы сообщаем о том,
+    # что каждая запись написана пользователем, и пользователь может создать сколько угодно постов(posts).
+    author = models.ForeignKey(User, related_name='blog_posts', on_delete=models.CASCADE)
+
+    # Это поле является полем TextField, которое преобразуется в текстовый столбец базы данных SQL
+    body = models.TextField()
+
+    # publish : этот параметр DateTime указывает, когда была опубликована запись.
+    publish = models.DateTimeField(default=timezone.now())
+
+    # Created : этот DateTime параметр указывает, когда был создан пост. Поскольку мы используем auto_now_add здесь,
+    # дата будет автоматически добавлена при создании объекта.
+    created = models.DateTimeField(auto_now_add=True)
+
+    # updated : этот DateTime параметр указывает на последний момент обновления этой записи.
+    updated = models.DateTimeField(auto_now=True)
+
+    # status : это поле для отображения статуса записи(опубликован\не опубликован).
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
 
 class Meta:
